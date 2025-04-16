ca6f86e91999c0f5a7ffa1e4b1eec9a55e439cf1
arnoldoocruz <arnoldoocruz96@gmail.com>
2025-04-15 22:58:12 -0400

    refactor

--- demo/src/reducers/main.js
+++ demo/src/reducers/main.js
@@ -7,19 +7,23 @@ import {
   SET_PROJECT_DETAIL_IMAGE_NUM,
   SET_PROJECT_SUMMARY_IMAGE_NUM,
   SET_SHOW_MENU,
-  SET_SHOW_LINK
-} from '../constants/actionTypes';
+  SET_SHOW_LINK,
+} from "../constants/actionTypes";
 
-export default (state = { navbarNum: 0, 
-                          displayProjectPanel: false, 
-                          showDetail: false, 
-                          homeBackNum: 0, 
-                          fifthShowDetail: false, 
-                          projectDetailImageNum: 0, 
-                          projectSummaryImageNum: 0,
-                          showMenu: false,
-                          showLink: false,
-                         }, action) => {
+export default (
+  state = {
+    navbarNum: 0,
+    displayProjectPanel: false,
+    showDetail: false,
+    homeBackNum: 0,
+    fifthShowDetail: false,
+    projectDetailImageNum: 0,
+    projectSummaryImageNum: 0,
+    showMenu: false,
+    showLink: false,
+  },
+  action,
+) => {
   switch (action.type) {
     case SET_NAVBAR_NUM:
       return {
@@ -30,42 +34,42 @@ export default (state = { navbarNum: 0,
       return {
         ...state,
         displayProjectPanel: action.payload,
-      }
+      };
     case SET_SHOW_DETAIL:
       return {
         ...state,
         showDetail: action.payload,
-      }
+      };
     case SET_HOME_BACK_NUM:
       return {
         ...state,
         homeBackNum: action.payload,
-      }
+      };
     case SET_FIFTH_SHOW_DETAIL:
       return {
         ...state,
         fifthShowDetail: action.payload,
-      }
+      };
     case SET_PROJECT_DETAIL_IMAGE_NUM:
       return {
         ...state,
         projectDetailImageNum: action.payload,
-      }
+      };
     case SET_PROJECT_SUMMARY_IMAGE_NUM:
       return {
         ...state,
         projectSummaryImageNum: action.payload,
-      }
+      };
     case SET_SHOW_MENU:
-      return{
+      return {
         ...state,
         showMenu: action.payload,
-      }
+      };
     case SET_SHOW_LINK:
-      return{
+      return {
         ...state,
         showLink: action.payload,
-      }
+      };
     default:
       return state;
   }
